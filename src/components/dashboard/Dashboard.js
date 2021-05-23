7dd9fbcfdf4e3e0920051cb0b9beeba5f563cf51
Данил Коротенко <dankorotenko@mbpro.local>
2021-05-23 16:35:52 +0200

    adding redux/reducers and dummy data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
-import ProjectList from "../projects/ProjectList"
-import Notifications from "./Notifications"
+import ProjectList from "../projects/ProjectList";
+import Notifications from "./Notifications";
+import { connect } from 'react-redux';
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+    const { projects } = props;
     return ( 
         <div className="dashboard container">
             <div className="row">
                 <div className="col s12 m6">
-                    <ProjectList />
+                    <ProjectList projects={projects}/>
                 </div>
                 <div className="col s12 m5 offset-m1">
                     <Notifications />
@@ -15,5 +17,11 @@ const Dashboard = () => {
         </div>
      );
 }
+
+const mapStateToProps = state => {
+    return {
+        projects: state.project.projects
+    }
+}
  
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
